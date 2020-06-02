6aee9dbb9dd8fdb3b06e80496007bf75eaeb386d
Juan Cruz <jjuancruzrey@gmail.com>
2020-06-02 18:03:45 -0300

    Obtiene Estado de coneccion y Muestra mejor GPS

--- CODIGO/KeepCalm/app/src/main/java/com/example/keepcalm/Get_Gps.java
+++ CODIGO/KeepCalm/app/src/main/java/com/example/keepcalm/Get_Gps.java
@@ -5,28 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class activity_get_gps extends AppCompatActivity {
+public class Get_Gps extends AppCompatActivity {
 
     Button btn_location;
     TextView textView1, textView2, textView3, textView4, textView5;
@@ -68,9 +72,22 @@ public class activity_get_gps extends AppCompatActivity {
         });
     }
 
+    private void getConectionState(){
+        //test de validacion de conexion a internet
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        Toast toast = Toast.makeText(this, "Estado de coneccion " +isConnected , Toast.LENGTH_SHORT);
+        toast.show();
+    }
     private void getLocation() {
+        getConectionState();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(activity_get_gps.this, new String[]{
+            ActivityCompat.requestPermissions(Get_Gps.this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION}, 44);
             return;
         }
@@ -83,7 +100,7 @@ public class activity_get_gps extends AppCompatActivity {
 
                     try {
                         //si obtuvo algo, inicializo Geocoder
-                        Geocoder geocoder = new Geocoder(activity_get_gps.this, Locale.getDefault());
+                        Geocoder geocoder = new Geocoder(Get_Gps.this, Locale.getDefault());
                         //Inicializo lista de direcciones
 
                         List<Address> direcciones;
@@ -98,20 +115,18 @@ public class activity_get_gps extends AppCompatActivity {
                         textView2.setText(Html.fromHtml("<font color='#6200EE'<b>Longitude</b><br></font>" +
                                 direcciones.get(0).getLongitude()
                         ));
-                        //set Longitud
-                        textView3.setText(Html.fromHtml("<font color='#6200EE'<b>Longitude</b><br></font>" +
-                                direcciones.get(0).getLongitude()
+                        //set Pais
+                        textView3.setText(Html.fromHtml("<font color='#6200EE'<b>Pais</b><br></font>" +
+                                direcciones.get(0).getCountryName()
                         ));
-                        //set Longitud
-                        textView4.setText(Html.fromHtml("<font color='#6200EE'<b>Longitude</b><br></font>" +
-                                direcciones.get(0).getLongitude()
+                        //set Localidad
+                        textView4.setText(Html.fromHtml("<font color='#6200EE'<b>Localidad</b><br></font>" +
+                                direcciones.get(0).getLocality()
                         ));
-                        //set Longitud
-                        textView5.setText(Html.fromHtml("<font color='#6200EE'<b>Longitude</b><br></font>" +
-                                direcciones.get(0).getLongitude()
+                        //set Addres Line
+                        textView5.setText(Html.fromHtml("<font color='#6200EE'<b>Addres Line</b><br></font>" +
+                                direcciones.get(0).getAddressLine(0)
                         ));
-                        Log.i("latitud", String.valueOf(direcciones.get(0).getLatitude()));
-                        Log.i("longitud", String.valueOf(direcciones.get(0).getLongitude()));
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
