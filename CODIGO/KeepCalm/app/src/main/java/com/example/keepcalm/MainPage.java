2b65b2cad11d142cd5b2df76861f9c91d0a0418f
FacundoChervin <facundo.chervin@gmail.com>
2020-06-03 21:27:43 -0300

    Reestructuracion del proyecto

--- CODIGO/KeepCalm/app/src/main/java/com/example/keepcalm/MainPage.java
+++ CODIGO/KeepCalm/app/src/main/java/com/example/keepcalm/MainPage.java
@@ -1,8 +1,10 @@
 package com.example.keepcalm;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
@@ -30,6 +32,7 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
@@ -52,9 +55,12 @@ public class MainPage extends AppCompatActivity {
     private TextView txtViewDia;
     private TextView txtViewHora;
     private Chronometer chronometer;
+    private TextView txtGps;
     private TextView txtViewTestNofShakes;
     private Button buttonOK;
     private Button buttonHistorial;
+
+
 
     private double latitude;
     private double longitude;
@@ -67,7 +73,6 @@ public class MainPage extends AppCompatActivity {
     private ShakeDetector mShakeDetector;
     private String token;
     private String userName;
-
     private Integer countShakes = 0;
 
     private static IntentFilter fechaFilter;
@@ -90,8 +95,8 @@ public class MainPage extends AppCompatActivity {
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
         unregisterReceiver(fechaListener);
+        super.onDestroy();
     }
 
     @Override
@@ -99,23 +104,40 @@ public class MainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-        fechaFilter = new IntentFilter();
-        fechaFilter.addAction(Intent.ACTION_DATE_CHANGED);
+        Bundle extras = getIntent().getExtras();
 
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
+        if (extras != null) {
+            this.token = extras.getString("USER_TOKEN");
+            this.userName = extras.getString("USER_NAME");
+        }
+        else {
+            //mostrar error!!
+        }
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainPage.this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
         }
+
+        fechaFilter = new IntentFilter();
+        fechaFilter.addAction(Intent.ACTION_DATE_CHANGED);
+        txtGps = findViewById(R.id.textViewGPS);
         txtViewDia = findViewById(R.id.textViewDia);
         chronometer = findViewById(R.id.chronometer);
         txtViewTestNofShakes = findViewById(R.id.textViewCountShakes);
         buttonOK = findViewById(R.id.buttonOK);
         buttonHistorial = findViewById(R.id.buttonHistorial);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        DateFormat df = new SimpleDateFormat("EEEE d 'de' MMMM 'del' yyyy");
+        String date = df.format(Calendar.getInstance().getTime()).toUpperCase();
+        txtViewDia.setText(date);
+
+
+
+
 
         buttonHistorial.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -123,18 +145,16 @@ public class MainPage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        buttonOK.setOnClickListener(new View.OnClickListener() {
 
+        buttonOK.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 getLocation();
-                txtViewTestNofShakes.setText(address);
+                txtGps.setText(address);
                 //chronometer.stop();
             }
         });
 
-        DateFormat df = new SimpleDateFormat("EEEE d 'de' MMMM 'del' yyyy");
-        String date = df.format(Calendar.getInstance().getTime()).toUpperCase();
-        txtViewDia.setText(date);
+
 
         fechaListener = new BroadcastReceiver() {
             @Override
@@ -146,12 +166,7 @@ public class MainPage extends AppCompatActivity {
         };
 
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            this.token = extras.getString("USER_TOKEN");
-            this.userName = extras.getString("USER_NAME");
-            //txtViewTest.setText("Token: "+this.token+"\n"+"User Name: "+this.userName+"\n");
-        }
+
 
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -162,19 +177,48 @@ public class MainPage extends AppCompatActivity {
 
             @Override
             public void onShake(int count) {
-                if (count == 1) {
+                countShakes++;
+                if (countShakes == 1) {
                     chronometer.start();
+                    getLocation();
+                    txtGps.setText(address);
+                    //Logear en el servidor el evento de inicio de shake con la ubicación del dispositivo
                 }
                 String text = "Shake Nº : " + String.format("%d", count);
                 txtViewTestNofShakes.setText(text);
                 Log.v("Shake Nº :", String.valueOf(count));
-
             }
 
             @Override
             public void onShakeStops(int count) {
                 countShakes = count;
-                handleShakeEvent();
+                Long elapsedMillis = SystemClock.elapsedRealtime() - chronometer.getBase();
+                chronometer.stop();
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainPage.this);
+                alert.setTitle("Doctor");
+                alert.setMessage("Su ataque de ... ha terminado?");
+                alert.setPositiveButton("¡Me encuentro bien!",new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getLocation();
+                        txtGps.setText(address);
+                        Log.v("Tiempo que paso", String.valueOf(elapsedMillis-5000));
+
+                        //handleShakeEvent();
+                    }
+                });
+                alert.setNegativeButton("¡Necesito ayuda!", null);
+                /*
+                alert.setNegativeButton("¡Necesito ayuda!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //En este caso llamar al contacto de emergencia
+                        //No se va a implementar en la entrega por el tema del tiempo
+
+                    }
+                });*/
+                alert.show();
             }
         });
         mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
@@ -224,7 +268,8 @@ public class MainPage extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainPage.this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-            Toast toast = Toast.makeText(this, "Debe permitir que la aplicación acceda a la ubicación del dispositivo", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Debe permitir que la aplicación acceda a la ubicación del dispositivo", Toast.LENGTH_SHORT);
+            Log.v("A ver si entro lrpm","si entro");
             return;
         }
         //getConectionState();
@@ -232,25 +277,16 @@ public class MainPage extends AppCompatActivity {
         fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
             @Override
             public void onComplete(@NonNull Task<Location> task) {
-                //
                 Location location = task.getResult();
                 if (location != null) {
-
                     try {
-                        //si obtuvo algo, inicializo Geocoder
                         Geocoder geocoder = new Geocoder(MainPage.this, Locale.getDefault());
-                        //Inicializo lista de direcciones
-
                         List<Address> direcciones;
                         direcciones = geocoder.getFromLocation(
                                 location.getLatitude(), location.getLongitude(), 1
                         );
-                        //set Latitud
                         latitude = direcciones.get(0).getLatitude();
-                        //set Longitud
                         longitude = direcciones.get(0).getLongitude();
-
-                        //set Addres Line
                         address = direcciones.get(0).getAddressLine(0);
                     } catch (IOException e) {
                         e.printStackTrace();
