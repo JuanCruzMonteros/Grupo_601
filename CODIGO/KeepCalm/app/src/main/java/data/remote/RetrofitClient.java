6e188832db4de87c10a274b59132b0a5d9f19b60
FacundoChervin <facundo.chervin@gmail.com>
2020-06-04 12:38:04 -0300

    Se extiende el CallTimeOut y readTimeOut para los Post request

--- CODIGO/KeepCalm/app/src/main/java/data/remote/RetrofitClient.java
+++ CODIGO/KeepCalm/app/src/main/java/data/remote/RetrofitClient.java
@@ -1,5 +1,8 @@
 package data.remote;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,11 +10,20 @@ public class RetrofitClient {
 
     private static Retrofit retrofit = null;
 
+
+
     public static Retrofit getClient(String baseUrl) {
+
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .callTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20,TimeUnit.SECONDS)
+                .build();
+
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
                     .build();
         }
         return retrofit;
